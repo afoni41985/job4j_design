2f3b928a0b7b648ceeffb3b63375652c781da32b
= <=afoni41985@yandex.ru>
2021-07-23 22:43:22 +0300

    5.2.2. Реализовать Store<T extends Base> [#157]

---
+++ src/main/java/ru/job4j/generics/MemStore.java
@@ -0,0 +1,33 @@
+package ru.job4j.generics;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class MemStore<T extends Base> implements Store<T> {
+
+    private final Map<String, T> mem = new HashMap<>();
+
+    @Override
+    public void add(T model) {
+        mem.put(model.getId(), model);
+    }
+
+    @Override
+    public boolean replace(String id, T model) {
+        return mem.replace(id, mem.get(id), model);
+    }
+
+    @Override
+    public boolean delete(String id) {
+      return mem.remove(id, mem.get(id));
+
+    }
+
+    @Override
+    public T findById(String id) {
+        if (mem.containsKey(id)) {
+            mem.get(id);
+        }
+        return null;
+    }
+}
