e2cb764c4af69ed49815148c909f4de486632e4f
= <=afoni41985@yandex.ru>
2021-08-02 07:14:11 +0300

    5. Очередь на двух стеках [#160 #183789]

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -1,27 +1,28 @@
 package ru.job4j.collection;
 
+
+import java.util.NoSuchElementException;
+
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
-    private int size = 0;
+
 
     public T poll() {
-        for (int i = 0; i < size; i++) {
-            out.push(in.pop());
+        if (in.isEmpty() && out.isEmpty()) {
+            throw new NoSuchElementException();
         }
-
-        T rsl = out.pop();
-        size--;
-        for (int i = 0; i < size; i++) {
-            in.push(out.pop());
+        if (out.isEmpty()) {
+            while (!in.isEmpty()) {
+                out.push(in.pop());
+            }
         }
-
-        return rsl;
+        return out.pop();
     }
+
 
     public void push(T value) {
         in.push(value);
-        size++;
     }
 }
 
