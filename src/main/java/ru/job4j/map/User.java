82d9487a1728216cbe6db52c49fba917e2525713
= <=afoni41985@yandex.ru>
2021-08-03 23:31:06 +0300

    2. Без переопределения equals и hashCode [#1005 #185582]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,9 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     private String name;
@@ -11,5 +14,14 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        User user1 = new User("aleks", 10, new GregorianCalendar(2011, Calendar.JUNE, 25));
+        User user = new User("aleks", 10, new GregorianCalendar(2011, Calendar.JUNE, 25));
+        var users = new HashMap<User, Object>();
+        users.put(user, new Object());
+        users.put(user1, new Object());
+        users.forEach((k, v) -> System.out.println(k + " " + v));
     }
 }
