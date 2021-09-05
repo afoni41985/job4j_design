3c27f21706a76345bfe139a5969e3e079df4a91e
= <=afoni41985@yandex.ru>
2021-09-05 13:13:41 +0300

    1. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,60 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<String, String>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            for (String str = read.readLine(); str != null; str = read.readLine()) {
+                if (str.isBlank() || str.startsWith("#")) {
+                    continue;
+                }
+                String[] rsl = str.split("=");
+                if (rsl.length != 2) {
+                    throw new IllegalArgumentException();
+                }
+                values.put(rsl[0], rsl[1]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        String rsl = null;
+        for (String k : values.keySet()) {
+            if (k.equals(key)) {
+                rsl = values.get(k);
+            }
+        }
+        return rsl;
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("app.properties"));
+    }
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.io;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name"), is("Petr Arsentev"));
+        assertThat(config.value("surname"), is(Matchers.nullValue()));
+    }
+
+    @Test
+    public void whenPairWithCommentsAndEmptyLines() {
+        String path = "./data/pair_with_comments_and_empty_lines.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name"), is("Petr Arsentev"));
+        assertThat(config.value("surname"), is(Matchers.nullValue()));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void whenPairNotTemplate() {
+        String path = "./data/when_pair_not_template.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+}
