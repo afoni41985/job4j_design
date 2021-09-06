a3225a794302abbac0fcfe7f747616b95d07eb1f
= <=afoni41985@yandex.ru>
2021-09-06 19:33:47 +0300

    3.0. Тестирование IO [#173905]

--- src/test/java/ru/job4j/io/AnalyzeTest.java
+++ src/test/java/ru/job4j/io/AnalyzeTest.java
@@ -1,28 +1,37 @@
 package ru.job4j.io;
 
-import org.hamcrest.Matchers;
-import org.junit.Test;
-
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-
-import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.*;
+import java.util.List;
 
 public class AnalyzeTest {
-    @Test
-    public void whenTwoRangeUnavailable() {
-        String source = "./project/server.log";
-        String target = "./project/unavailable.csv";
-        Analyze analyze = new Analyze();
-        analyze.unavailable(source, target);
-        try (var in = new BufferedReader(new FileReader(target))) {
-            assertThat(in.readLine(), is("10:57:01;10:59:01;"));
-            assertThat(in.readLine(), is("11:01:02;11:02:02;"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    @Rule
+    public TemporaryFolder fold = new TemporaryFolder();
 
+    @Test
+    public void unavailable() throws IOException {
+        File source = fold.newFile("source.txt");
+        File target = fold.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01\n"
+                    + "500 10:57:01\n"
+                    + "400 10:58:01\n"
+                    + "200 10:59:01\n"
+                    + "500 11:01:02\n"
+                    + "200 11:02:02");
+        }
+        Analyze analyze = new Analyze();
+        analyze.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+        assertThat(rsl.toString(), is("10:57:01;10:59:01;" + "11:01:02;11:02:02;"));
+    }
 }
