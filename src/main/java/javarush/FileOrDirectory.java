44cf060bc2a929706ff9d491242b7ce2f564f70e
= <=afoni41985@yandex.ru>
2021-09-07 17:31:19 +0300

    JavaRush - FileOrDirectory

---
+++ src/main/java/javarush/FileOrDirectory.java
@@ -0,0 +1,30 @@
+package javarush;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class FileOrDirectory {
+    private static final String THIS_IS_FILE = " - это файл";
+    private static final String THIS_IS_DIR = " - это директория";
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        //напишите тут ваш код
+        boolean stop = false;
+        while (!stop) {
+            Path directory = Path.of(scanner.nextLine());
+            stop = true;
+            if (Files.isDirectory(directory)) {
+                stop = false;
+                System.out.println(directory + THIS_IS_DIR);
+            } else if (Files.isRegularFile(directory)) {
+                stop = false;
+                System.out.println(directory + THIS_IS_FILE);
+            }
+
+        }
+
+    }
+}
