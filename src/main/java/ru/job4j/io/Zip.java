afe66b4959a1efa6f2b452cbcccc2ffcc61253b9
= <=afoni41985@yandex.ru>
2021-09-15 15:37:37 +0300

    5.2. Архивировать проект [#861 #201284]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.zip.ZipEntry;
@@ -10,19 +11,23 @@ import java.util.zip.ZipOutputStream;
 public class Zip {
 
     public static void packFiles(List<Path> sources, File target) {
-        if (sources.isEmpty()) {
-            throw new IllegalArgumentException();
-        }
-        if (!target.exists()) {
-            throw new IllegalArgumentException();
-        }
-        for (Path elem : sources) {
-            packSingleFile(elem.toFile(), target);
+        try (ZipOutputStream zip = new ZipOutputStream(
+                new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (Path path : sources) {
+                zip.putNextEntry(new ZipEntry(path.toString()));
+                try (BufferedInputStream out = new BufferedInputStream(
+                        new FileInputStream(sources.toString()))) {
+                    zip.write(out.readAllBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
     public static void packSingleFile(File source, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+        try (ZipOutputStream zip = new ZipOutputStream(
+                new BufferedOutputStream(new FileOutputStream(target)))) {
             zip.putNextEntry(new ZipEntry(source.getPath()));
             try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
                 zip.write(out.readAllBytes());
@@ -32,10 +37,22 @@ public class Zip {
         }
     }
 
-    public static void main(String[] args) {
-        packSingleFile(
-                new File("./chapter_005/pom.xml"),
-                new File("./chapter_005/pom.zip")
-        );
+    public static void main(String[] args) throws IOException {
+        if (args.length < 3) {
+            throw new IllegalArgumentException("Invalid arguments "
+                    + "Usage java "
+                    + "-jar pack.jar -d=DIRECTORY_ARCHIVE -e=EXCLUDE_EXCLUDE -o=OUTPUT_ARCHIVE");
+        }
+        ArgsName argsName = ArgsName.of(args);
+        Path root = Path.of(argsName.get("d"));
+        if (!Files.exists(root)) {
+            throw new IllegalArgumentException("folder does not exist");
+        }
+        String extent = argsName.get("e");
+        File target = new File(argsName.get("o"));
+        List<Path> sources = Search.search(root, path ->
+                !path.toFile().getName().endsWith(extent));
+        packFiles(sources, target);
+
     }
 }
