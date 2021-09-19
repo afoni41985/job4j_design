b5c881b10ba3d6a31ea1227661c05046b3614abf
= <=afoni41985@yandex.ru>
2021-09-19 20:23:19 +0300

    2. Simple Loggin Facade 4 Java. [#268849]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -1,4 +1,5 @@
 package ru.job4j.io;
+
 import org.slf4j.Logger;
 
 import org.slf4j.LoggerFactory;
@@ -8,10 +9,17 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Petr Arsentev";
+        int age = 33;
+        boolean valid = true;
+        float weight = 80.0f;
+        char gender = 'm';
+        short worker = 10;
+        long height = 190L;
+        double salary = 12345.6789;
+        byte child = 1;
+        LOG.debug("User info name : {}, age {}, valid {}, weight {}, gender {}, worker {}, height {}, salary {}, child {}",
+                name, age, valid, weight, gender, worker, height, salary, child);
+
     }
 }
