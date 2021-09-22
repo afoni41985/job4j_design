68aa4196fad1b87eb69a527376e43da606f0dff6
= <=afoni41985@yandex.ru>
2021-09-22 21:17:59 +0300

    7. Scanner [#504791]

---
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -0,0 +1,75 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+import java.nio.file.Path;
+import java.util.*;
+import java.util.stream.IntStream;
+
+import ru.job4j.io.ArgsName.*;
+
+public class CSVReader {
+    private final Path path;
+    private final String delimiter;
+    private final String out;
+    private final String filter;
+
+    public CSVReader(ArgsName argsName) {
+        this.path = Path.of(argsName.get("path"));
+        this.delimiter = argsName.get("delimiter");
+        this.out = argsName.get("out");
+        this.filter = argsName.get("filter");
+    }
+
+    public void reader() throws IOException {
+        StringBuilder outPrint = resultOut(getArgsFilter(filter));
+        if (out.equals("stdout")) {
+            System.out.println(outPrint);
+        } else {
+            try (FileWriter writer = new FileWriter(out)) {
+                writer.write(String.valueOf(outPrint));
+            }
+        }
+    }
+
+    private int[] getArgsFilter(String filter) throws IOException {
+        Scanner scanner = new Scanner(path).useDelimiter(System.lineSeparator());
+        String[] columns = scanner.next().split(delimiter);
+        return IntStream.range(0, columns.length)
+                .filter(i -> filter.contains(columns[i])).toArray();
+
+    }
+
+    private StringBuilder resultOut(int[] indexFilter) {
+        StringBuilder result = new StringBuilder();
+        try (Scanner scArgs = new Scanner(path).useDelimiter(System.lineSeparator())) {
+            while (scArgs.hasNextLine()) {
+                List<String> outColumns = List.of(scArgs.next().split(delimiter));
+                for (int i = 0; i < indexFilter.length; i++) {
+                    result.append(outColumns.get(indexFilter[i]));
+                    if (i != indexFilter.length - 1) {
+                        result.append(";");
+                    }
+                }
+                result.append(System.lineSeparator());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static void checkArgs(String[] args) {
+        if (args.length != 4) {
+            throw new IllegalArgumentException(
+                    "Invalid arguments"
+            );
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        checkArgs(args);
+        CSVReader csvReader = new CSVReader(ArgsName.of(args));
+        csvReader.reader();
+    }
+}
