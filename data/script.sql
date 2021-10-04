404d72833f69a786d7d28ca148f22128f39276e3
= <=afoni41985@yandex.ru>
2021-10-04 14:35:59 +0300

    0. Что такое база данных. [#6860]

---
+++ data/script.sql
@@ -0,0 +1,7 @@
+select * from car;
+insert into car(price,color,broken) values ('5000.24','red','true');
+select * from car;
+update car set price = '0.0';
+select * from car;
+delete from car;
+select * from car;
