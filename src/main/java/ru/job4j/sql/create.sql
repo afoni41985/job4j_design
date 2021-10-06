12d299eb54a5755baa3b718603c6b32ced5fcf29
= <=afoni41985@yandex.ru>
2021-10-06 22:44:33 +0300

    1. Схема прав пользователи и роли. [#1093]

---
+++ src/main/java/ru/job4j/sql/create.sql
@@ -0,0 +1,50 @@
+create table users(
+id serial primary key,
+name_user varchar(100),
+role_id int references role(id)
+);
+
+create table role(
+id serial primary key,
+roles varchar(100)
+);
+
+create table rules(
+id serial primary key,
+rules varchar(100)
+);
+
+create table role_rules(
+id serial primary key,
+role_id int references role(id),
+rules_id int references rules(id)
+);
+
+create table item(
+id serial primary key,
+user_id int references users(id),
+category_id int references category(id),
+state_id int references state(id)
+);
+
+create table comments(
+id serial primary key,
+comments text,
+item_id int references item(id)
+);
+
+create table attachs(
+id serial primary key,
+attachs varchar(100),
+item_id int references item(id)
+);
+
+create table state(
+id serial primary key,
+state varchar(100)
+);
+
+create table category(
+id serial primary key,
+category varchar(100)
+);
---
+++ src/main/java/ru/job4j/sql/insert.sql
@@ -0,0 +1,23 @@
+insert into role(roles) values ('админ'),('пользoватель'),('гость');
+
+insert into category(category) values ('срочная'),('текущая'),('просроченная');
+
+insert into state(state) values ('новая'),('выполнена');
+
+insert into attachs(attachs, item_id) values ('a',1),('b',2),('c',3);
+
+insert into rules(rules) values ('Создавать заявки'),('Редактировать заявки'),
+                                ('Просматривать заявки'),('Удалять заявки');
+insert into users(name_user,role_id) values ('Вася',1),('Коля',2),('Дима',3);
+
+insert into role_rules(role_id, rules_id) values (1, 1);
+insert into role_rules(role_id, rules_id) values (2, 2);
+insert into role_rules(role_id, rules_id) values (3, 3);
+
+insert into item(user_id, category_id, state_id) values (1, 3, 1);
+insert into item(user_id, category_id, state_id) values (2, 2, 3);
+insert into item(user_id, category_id, state_id) values (3, 1, 2);
+
+insert into comments(comments, item_id) values ('aaa', 1);
+insert into comments(comments, item_id) values ('bbb', 3);
+insert into comments(comments, item_id) values ('ccc', 3);
