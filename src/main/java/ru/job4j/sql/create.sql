ab6cd0254ea3566dd5cc6aa02f958f1b6148d24e
= <=afoni41985@yandex.ru>
2021-10-08 19:31:50 +0300

    1. Схема прав пользователи и роли. [#1093 #213013]

--- src/main/java/ru/job4j/sql/create.sql
+++ src/main/java/ru/job4j/sql/create.sql
@@ -1,12 +1,12 @@
+create table role(
+id serial primary key,
+roles varchar(100)
+);
+
 create table users(
 id serial primary key,
 name_user varchar(100),
 role_id int references role(id)
-);
-
-create table role(
-id serial primary key,
-roles varchar(100)
 );
 
 create table rules(
@@ -18,6 +18,16 @@ create table role_rules(
 id serial primary key,
 role_id int references role(id),
 rules_id int references rules(id)
+);
+
+create table state(
+id serial primary key,
+state varchar(100)
+);
+
+create table category(
+id serial primary key,
+category varchar(100)
 );
 
 create table item(
@@ -39,12 +49,3 @@ attachs varchar(100),
 item_id int references item(id)
 );
 
-create table state(
-id serial primary key,
-state varchar(100)
-);
-
-create table category(
-id serial primary key,
-category varchar(100)
-);
--- src/main/java/ru/job4j/sql/insert.sql
+++ src/main/java/ru/job4j/sql/insert.sql
@@ -10,24 +10,26 @@ insert into rules(rules) values ('Создавать заявки'),('Редак
 insert into role_rules(role_id, rules_id) values (1, 1);
 insert into role_rules(role_id, rules_id) values (2, 2);
 insert into role_rules(role_id, rules_id) values (3, 3);
-insert into role_rules(role_id, rules_id) values (4, 4);
+
+insert into role_rules(role_id, rules_id) values (1, 1);
+insert into role_rules(role_id, rules_id) values (1, 3);
+insert into role_rules(role_id, rules_id) values (1, 4);
+insert into role_rules(role_id, rules_id) values (1, 3);
 
 insert into role_rules(role_id, rules_id) values (2, 1);
 insert into role_rules(role_id, rules_id) values (2, 3);
 insert into role_rules(role_id, rules_id) values (2, 4);
 insert into role_rules(role_id, rules_id) values (3, 3);
 
-insert into role_rules(role_id, rules_id) values (48, 35);
-
 insert into users(name_user,role_id) values ('Вася',1),('Коля',2),('Дима',3);
 
 insert into item(user_id, category_id, state_id) values (1, 3, 1);
-insert into item(user_id, category_id, state_id) values (2, 2, 3);
+insert into item(user_id, category_id, state_id) values (2, 2, 2);
 insert into item(user_id, category_id, state_id) values (3, 1, 2);
 
 insert into attachs(attachs, item_id) values ('a',1),('b',2),('c',3);
 
 insert into comments(comments, item_id) values ('aaa', 1);
-insert into comments(comments, item_id) values ('bbb', 3);
+insert into comments(comments, item_id) values ('bbb', 2);
 insert into comments(comments, item_id) values ('ccc', 3);
 
