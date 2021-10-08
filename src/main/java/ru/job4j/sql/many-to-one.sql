22dbd339eb70f7acbed6c4e3d5e34d4a7f8c9039
= <=afoni41985@yandex.ru>
2021-10-08 11:40:07 +0300

    0.1. Связи между таблицами [#373615 #212884](corrected)

--- src/main/java/ru/job4j/sql/many-to-one.sql
+++ src/main/java/ru/job4j/sql/many-to-one.sql
@@ -1,20 +1,19 @@
-create table pet(
-id serial primary key,
-nickname varchar(10)
-);
-
 create table owner(
 id serial primary key,
-names varchar(10),
-pet_id int references pet(id)
+names varchar(10)
 );
 
-insert into pet(nickname) values ('java');
-insert into pet(nickname) values ('sql');
-insert into owner(names,pet_id) values ('alex',1);
+create table pet(
+id serial primary key,
+nickname varchar(10),
+owner_id int references owner(id)
+);
+
+insert into owner(names) values ('alex');
+insert into pet(nickname, owner_id) values ('sql',1);
+insert into pet(nickname,owner_id) values ('java',1);
 
 select * from owner;
 select * from pet;
 select * from pet where id in (select id from owner);
-insert into owner(names,pet_id) values ('alex',2);
-select * from pet where id in (select id from owner);
+
