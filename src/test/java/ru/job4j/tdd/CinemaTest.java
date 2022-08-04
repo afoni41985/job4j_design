75a0981a343bbc2f58ed6fee76f27aef00cd4b84
= <=afoni41985@yandex.ru>
2022-08-04 18:33:12 +0300

    2. Что такое TDD? [#4918 #328269]

--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -68,8 +68,9 @@ public class CinemaTest {
         Account account = new AccountCinema();
         Cinema cinema = new Cinema3D();
         Calendar date = Calendar.getInstance();
+        cinema.buy(account, 1, 1, date);
         assertThrows(IllegalArgumentException.class, () -> {
-            cinema.buy(account, -1, 1, date);
+            cinema.buy(account, 1, 1, date);
         });
     }
 }
