7c127836a0f4548658f618304aa0c8aa2b9d69c7
= <=afoni41985@yandex.ru>
2023-04-06 21:56:03 +0300

    1. Отчеты. [#850]

---
+++ src/test/java/ru/job4j/ood/srp/report/ReportAccountingTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.ood.srp.report;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.ood.srp.currency.Currency;
+import ru.job4j.ood.srp.currency.InMemoryCurrencyConverter;
+import ru.job4j.ood.srp.formatter.DateTimeParser;
+import ru.job4j.ood.srp.formatter.ReportDateTimeParser;
+import ru.job4j.ood.srp.model.Employee;
+import ru.job4j.ood.srp.store.MemStore;
+import ru.job4j.ood.srp.store.Store;
+
+import java.util.Calendar;
+import java.util.function.Predicate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReportAccountingTest {
+
+    @Test
+    public void whenOldGenerated() {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employee worker = new Employee("Ivan", now, now, 100);
+        DateTimeParser<Calendar> parser = new ReportDateTimeParser();
+        store.add(worker);
+        ReportAccounting engine = new ReportAccounting(store, parser);
+        InMemoryCurrencyConverter converter = new InMemoryCurrencyConverter();
+        String expect = "Name; Hired; Fired; Salary;" +
+                System.lineSeparator() +
+                worker.getName() + " " +
+                parser.parse(worker.getHired()) + " " +
+                parser.parse(worker.getFired()) + " " +
+                converter.convert(Currency.USD,worker.getSalary(),Currency.RUB) +
+                System.lineSeparator();
+        assertThat(engine.generate(em -> true)).isEqualTo(expect);
+    }
+}
---
+++ src/test/java/ru/job4j/ood/srp/report/ReportHRTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.ood.srp.report;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.ood.srp.formatter.DateTimeParser;
+import ru.job4j.ood.srp.formatter.ReportDateTimeParser;
+import ru.job4j.ood.srp.model.Employee;
+import ru.job4j.ood.srp.store.MemStore;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Calendar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReportHRTest {
+
+    @Test
+    public void whenOldGenerated() {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employee worker1 = new Employee("Ivan", now, now, 1200);
+        Employee worker2 = new Employee("Taran", now, now, 18000);
+        Employee worker3 = new Employee("Divan", now, now, 200);
+        store.add(worker1);
+        store.add(worker2);
+        store.add(worker3);
+        DateTimeParser<Calendar> parser = new ReportDateTimeParser();
+        ReportHR hr = new ReportHR(store, parser);
+        String expect = "Name; Salary;" +
+                System.lineSeparator() +
+                worker2.getName() + " " +
+                worker2.getSalary() +
+                System.lineSeparator() +
+                worker1.getName() + " " +
+                worker1.getSalary() +
+                System.lineSeparator() +
+                worker3.getName() + " " +
+                worker3.getSalary() +
+                System.lineSeparator();
+        assertThat(hr.generate(em -> true)).isEqualTo(expect);
+
+    }
+}
